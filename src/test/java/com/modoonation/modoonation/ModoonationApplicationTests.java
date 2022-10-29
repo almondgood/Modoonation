c22ce2d1cff25d56409bf6f42c99c8248d035fda
AlmondGood <jwt2358@naver.com>
2022-10-29 19:56:53 +0900

    초기 설정

---
+++ src/test/java/com/modoonation/modoonation/ModoonationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modoonation.modoonation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModoonationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
